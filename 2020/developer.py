388aef4cd8296ca4e5cd26a791375258cb8eb60e
matzof <45333181+matzof@users.noreply.github.com>
2020-03-12 19:18:12 +0200

    Update developer.py

--- 2020/developer.py
+++ 2020/developer.py
@@ -9,20 +9,24 @@ class Developer:
     def toString(self):
         return f"Company: {self.ci} | Bonus: {self.bi} | Skills: {self.skills}"
     
-    def compareDev(self, person): 
-        common = len(self.skills.intersection(person.skills))
-        diff = len(self.skills.union(person.skills)) - common
-        work_score = common * diff
-        bonus_score = self.bi * person.bi  
-        return work_score + bonus_score
-#        except: 
-#            return self.bi * person.bi  
+    def compareDev(self, person):
+        try:
+            common = len(self.skills.intersection(person.skills))
+            diff = len(self.skills.union(person.skills)) - common
+            work_score = common * diff
+            bonus_score = self.bi * person.bi  
+            return work_score + bonus_score
+        except: 
+            return self.bi * person.bi  
 
 class Manager:
     def __init__(self, line):
-        line_content = line.split(' ')
+        line_content = line.rstrip().split(' ')
         self.ci = line_content[0]
-        self.bi = line_content[1]
+        self.bi = int(line_content[1])
 
+    def compareManager(self, person):
+        return self.bi * person.bi
+    
     def toString(self):
         return f"Company: {self.ci} | Bonus: {self.bi}"
