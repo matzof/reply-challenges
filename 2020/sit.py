ff4f5ab1b022f0a7dba18a91d09231bc00f4b846
matzof <45333181+matzof@users.noreply.github.com>
2020-03-12 20:32:10 +0200

    Update sit.py

--- 2020/sit.py
+++ 2020/sit.py
@@ -4,6 +4,32 @@ class Sit():
         self.y = int(y)
         self.type = type
         self.taken = False
+        self.person = None
 
     def toString(self):
         return f"{self.x}:{self.y} = {self.type}"
+    
+    def getNeighbours(self, sit_list, width, height):  
+        neighbours = []
+        if self.y > 0:
+            person = sit_list[(self.y-1)*(width) + self.x].person
+            if person != None:
+                neighbours.append(person)
+
+        if self.y < height -1:
+            person = sit_list[(self.y+1)*(width) + self.x].person
+            if person != None:
+                neighbours.append(person)
+       
+        if self.x > 0:
+            person = sit_list[(self.y)*(width) + (self.x - 1)].person
+            if person != None:
+                neighbours.append(person)
+        
+        if self.x < width -1:
+            person = sit_list[(self.y+1)*(width) + (self.x + 1)].person
+            if person != None:
+                neighbours.append(person)
+                
+        return neighbours 
+            
